167dc51dc258e51a311a5b330d3d9d8ef0eaa758
MyInf272prep <inf272prep@gmail.com>
2022-11-26 09:50:07 +0200

    Add files via upload

---
+++ AsyncController.cs
@@ -0,0 +1,29 @@
+﻿using INF272EXAM20221126.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace INF272EXAM20221126.Controllers
+    {
+    public class AsyncController : Controller
+        {
+        public SchoolEntities db = new SchoolEntities();
+
+        public ActionResult Async()
+            {
+
+              return View();
+            }
+
+        // Complete the question for this section BELOW THIS LINE.
+        // Do not remove the green line.
+        // -------------------------------------------------------
+        // -------------------------------------------------------
+
+        public async Task<ActionResult> GetDepartment()
+            {
+                 return Json(await db.Departments.ToListAsync(), JsonRequestBehavior.AllowGet); 
+            }
+        }
+    }
